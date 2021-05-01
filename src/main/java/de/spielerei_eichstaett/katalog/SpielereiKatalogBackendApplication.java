d4cebc595b10b52c137ebc58ddde5aed66e55bf4
user <user@example.com>
2021-05-01 11:46:08 +0200

    init spring

---
+++ src/main/java/de/spielerei_eichstaett/katalog/SpielereiKatalogBackendApplication.java
@@ -0,0 +1,13 @@
+package de.spielerei_eichstaett.katalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpielereiKatalogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpielereiKatalogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/spielerei_eichstaett/katalog/SpielereiKatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.spielerei_eichstaett.katalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpielereiKatalogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
