0dd0b4d0025e66425b42f9983f25d1faee960bc0
user <user@example.com>
2021-05-07 12:52:47 +0200

    Enable @Preauthorize annotation and secure spiel repo with it

--- src/main/java/de/spielerei_eichstaett/katalog/security/WebSecurityConfig.java
+++ src/main/java/de/spielerei_eichstaett/katalog/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package de.spielerei_eichstaett.katalog.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,12 +11,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        manager.createUser(User.withDefaultPasswordEncoder()
+                                   .username("user")
+                                   .password("user")
+                                   .roles("USER")
+                                   .build());
+        manager.createUser(User.withDefaultPasswordEncoder()
+                                   .username("admin")
+                                   .password("admin")
+                                   .roles("USER", "ADMIN")
+                                   .build());
         return manager;
     }
 
--- src/main/java/de/spielerei_eichstaett/katalog/web/SpielRepository.java
+++ src/main/java/de/spielerei_eichstaett/katalog/web/SpielRepository.java
@@ -3,7 +3,33 @@ package de.spielerei_eichstaett.katalog.web;
 import de.spielerei_eichstaett.katalog.db.Spiel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
-@RepositoryRestResource(path = "spiele")
+import java.util.Optional;
+
+@PreAuthorize("hasRole('USER')")
+@RepositoryRestResource(
+        collectionResourceRel = "spiele",
+        path = "spiele"
+)
 public interface SpielRepository extends CrudRepository<Spiel, Integer> {
+    @PreAuthorize("permitAll()")
+    @Override
+    Optional<Spiel> findById(Integer integer);
+
+    @PreAuthorize("permitAll()")
+    @Override
+    boolean existsById(Integer integer);
+
+    @PreAuthorize("permitAll()")
+    @Override
+    Iterable<Spiel> findAll();
+
+    @PreAuthorize("permitAll()")
+    @Override
+    Iterable<Spiel> findAllById(Iterable<Integer> integers);
+
+    @PreAuthorize("permitAll()")
+    @Override
+    long count();
 }
